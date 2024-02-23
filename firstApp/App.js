b3254ea4832d9d66fbafc0c9d6ea6631459cc412
Caike <125682254+CaikeHenry007@users.noreply.github.com>
2024-02-23 12:34:43 -0300

    pronto

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,30 +1,44 @@
+// Importando todos os elementos que eu vou usar
 import { StyleSheet, Text, View, TextInput, Button, Image, ImageBackground } from 'react-native';
 
+// constante do alerta ao clicar no botão
 export default function App() {
 
   pressButton = () => {
     alert("Seja bem vindo!")
   }
-
+  
+  // Imagem de fundo
   const imagem = { uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLMzx4_RHZAx22XUXoq7d1BkTBsRfRwBk6Rw&usqp=CAU' }
 
+  // codigo do "body" do meu site 
   return (
+    // View principal do meu site
     <View style={styles.container}>
-      <ImageBackground source={imagem} style={styles.image} resizeMode='repeat'>
 
-        <Image require style={styles.image2} resizeMode='cover'
+      {/* Imagem de fundo */}
+      <ImageBackground source={imagem} 
+      style={styles.image} 
+      resizeMode='repeat'>
+
+{/* Imagem menor */}
+        <Image source={require('./assets/escudo_sp-removebg-preview.png')}
+        style={styles.image2} 
+        resizeMode='cover'
         />
 
+{/* Texto que fica sobre as caixas de texto */}
         <Text style={{ color: 'white', fontSize: 20, }}>Faça Login
         </Text>
 
-
+{/* Caixa de texto: Usuário */}
         <TextInput style={styles.inputName}
           placeholder='Usuário'
           inputMode='text'
           placeholderTextColor='black'
         />
 
+{/* Caixa de texto: Senha */}
         <TextInput style={styles.inputName}
           placeholder='Senha'
           inputMode='numeric'
@@ -32,11 +46,12 @@ export default function App() {
           placeholderTextColor='black'
         />
 
+{/* Botão: Entrar */}
         <Button
           style={{ backgroundColor: 'white', width: '50%', }}
           onPress={pressButton}
           title='Entrar!'
-          color='black'
+          color='red'
         />
 
 
@@ -45,6 +60,7 @@ export default function App() {
   );
 }
 
+// style do site
 const styles = StyleSheet.create({
   container: {
     flex: 1,
