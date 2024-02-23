d2ace34c2f29056d663eb2cc53bcfd5ecbf46f9c
Caike <125682254+CaikeHenry007@users.noreply.github.com>
2024-02-23 12:13:32 -0300

    ok

--- firstApp/App.js
+++ firstApp/App.js
@@ -1,30 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image, ImageBackground } from 'react-native';
 
 export default function App() {
-return (
-<View style={styles.container}>
-<View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
-</View>
-<View style={styles.secondaryContainer}>
-<Text style={{ color: 'blue',  fontSize: 30, fontWeight: 'bold', textAlign: 'right'}}>Caike Henry Firmino Felipe</Text>
-<Text style={{ color: 'black',fontSize: 25, fontStyle: 'italic', borderBottomColor: 'red', borderBottomWidth: 1, width: 180, textAlign: 'left'}}>Computador 6</Text>
-<Text style={{ color: 'black', fontSize: 20, backgroundColor: 'pink', textAlign: 'center',}}>Sala 7</Text>
-<Text style={{ color: 'black', fontSize: 20, backgroundColor: '#8fffbc', borderBottomColor: 'blue', borderBottomWidth: 1, textAlign: 'center'}}>Outro texto</Text>
-</View>
-<View style={{ backgroundColor: 'gray', width: '100%', height: 60 }}>
-</View>
-</View>
-);
+
+  pressButton = () => {
+    alert("Seja bem vindo!")
+  }
+
+  const imagem = { uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLMzx4_RHZAx22XUXoq7d1BkTBsRfRwBk6Rw&usqp=CAU' }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={imagem} style={styles.image} resizeMode='repeat'>
+
+        <Image require style={styles.image2} resizeMode='cover'
+        />
+
+        <Text style={{ color: 'white', fontSize: 20, }}>Faça Login
+        </Text>
+
+
+        <TextInput style={styles.inputName}
+          placeholder='Usuário'
+          inputMode='text'
+          placeholderTextColor='black'
+        />
+
+        <TextInput style={styles.inputName}
+          placeholder='Senha'
+          inputMode='numeric'
+          secureTextEntry={true}
+          placeholderTextColor='black'
+        />
+
+        <Button
+          style={{ backgroundColor: 'white', width: '50%', }}
+          onPress={pressButton}
+          title='Entrar!'
+          color='black'
+        />
+
+
+      </ImageBackground>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-},
-secondaryContainer: {
-flex: 1,
-backgroundColor: '#fff',
-justifyContent: 'center'
-},
+  container: {
+    flex: 1,
+  },
+  inputName: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 15,
+    fontSize: 20,
+    backgroundColor: 'white',
+    width: '50%',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image2: {
+    width: '20%',
+    height: '10%',
+  }
+
 });
