03eb31af3f9e6a8d5595d72c09c7dd67264c2a4f
Caike <125682254+CaikeHenry007@users.noreply.github.com>
2024-02-28 07:50:58 -0300

    ok

--- firstApp/App.js
+++ firstApp/App.js
@@ -70,6 +70,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  
+  image: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  image2: {
+    width: '20%',
+    height: '10%',
+  },
+
   inputName: {
     borderWidth: 1,
     borderColor: 'black',
@@ -78,15 +91,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     width: '50%',
   },
-  image: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image2: {
-    width: '20%',
-    height: '10%',
-  }
+  
 
 });
